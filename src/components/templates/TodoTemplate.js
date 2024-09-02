e8ffec18691eb2a628ce8452b8591b49d0b05163
Taiki Sano <taiki_sano@kronos.jp>
2024-09-02 17:11:26 +0900

    useEffectを使うように修正

--- src/components/templates/TodoTemplate.js
+++ src/components/templates/TodoTemplate.js
@@ -1,15 +1,20 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import TodoForm from '@/components/organisms/TodoForm'
 import TodoList from '@/components/organisms/TodoList'
 import { getTodos, saveTodos } from '@/utils/storage'
 
 export default function TodoTemplate() {
-  // TODO一覧をローカルストレージから取得
-  // todosに値が追加されたら再描画させたいのでStateとして扱う
-  const initialState = getTodos()
-  const [todos, setTodos] = useState(initialState)
+  const [todos, setTodos] = useState([])
+
+  // useEffectはレンダリング後に実行される
+  useEffect(() => {
+    // TODO一覧をローカルストレージから取得
+    // todosに値が追加されたら再描画させたいのでStateとして扱う
+    const storedTodos = getTodos()
+    setTodos(storedTodos)
+  }, [])
 
   // 追加処理
   // 引き数に新規TODOを受け取る
